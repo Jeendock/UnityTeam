8e723824e85dfa124ebd8834cdf9545829c29cbb
Jeendock <Jeendock@gmail.com>
2021-06-11 10:51:12 +0900

    EnemyRoot

---
+++ RBR/Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int hp { get; set; }
+
+    public Enemy()
+    {
+        hp = 10;
+    }
+
+    public void Hit(int Damage = 1)
+    {
+        hp -= Damage;
+    }
+
+    protected virtual void Attack()
+    {
+    }
+}
